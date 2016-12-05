95108f2ec4e08d6af5b37c708679f0bb3359af6c
Mu Hyung Park <admin@pmhtech.net>
2016-12-05 15:31:03 +0900

    Exceptionhandler 수정

--- sys/src/main/java/net/pmhtech/advice/ExceptionAdvice.java
+++ sys/src/main/java/net/pmhtech/advice/ExceptionAdvice.java
@@ -1,22 +1,13 @@
 package net.pmhtech.advice;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONObject;
-import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,17 +15,18 @@ import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 
 import net.pmhtech.user.exception.UserNotFoundException;
 import net.pmhtech.user.exception.UserPasswordMismatchedException;
-import net.pmhtech.view.AjaxXmlView;
 
 @ControllerAdvice
 public class ExceptionAdvice extends AbstractHandlerExceptionResolver{
 	  
 
 	@ResponseBody
-	@ResponseStatus(value = HttpStatus.FORBIDDEN,reason = "UserNotFoundException")
+	@ResponseStatus(value = HttpStatus.FORBIDDEN)
 	@ExceptionHandler(Exception.class)
     public ModelMap defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
         
+		e.printStackTrace();
+		
 		ModelMap modelMap = new ModelMap();
 		modelMap.addAttribute("code", "DDD");
 		modelMap.addAttribute("reason",e.getMessage());
@@ -42,12 +34,12 @@ public class ExceptionAdvice extends AbstractHandlerExceptionResolver{
 		return modelMap;
     }
 	@ResponseBody
-	@ResponseStatus(value = HttpStatus.FORBIDDEN,reason = "UserNotFoundException")
+	@ResponseStatus(value = HttpStatus.FORBIDDEN)
 	@ExceptionHandler(UserNotFoundException.class)
 	public ModelMap handlerUserNotFoundException(HttpServletRequest request, 
 			UserNotFoundException e){
 	
-
+		e.printStackTrace();
 		ModelMap modelMap = new ModelMap();
 		modelMap.addAttribute("code", "DDD");
 		modelMap.addAttribute("reason",e.getMessage());
@@ -61,7 +53,7 @@ public class ExceptionAdvice extends AbstractHandlerExceptionResolver{
 	@ExceptionHandler(UserPasswordMismatchedException.class)
 	public  ModelMap handlerUserPasswordUnMatchedException(HttpServletRequest request, 
 			UserPasswordMismatchedException e){
-		
+		e.printStackTrace();
 		ModelMap modelMap = new ModelMap();
 		modelMap.addAttribute("code", "DDD");
 		modelMap.addAttribute("reason",e.getMessage());
