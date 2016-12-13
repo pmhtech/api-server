7497c87c9ea37945dde0a487069d0accd4eee2d2
Muhyung Park <contact@pmhtech.net>
2016-12-14 00:21:24 +0900

    테스트용.. 커밋..

--- sys/src/main/java/net/pmhtech/advice/JsonpAdvice.java
+++ sys/src/main/java/net/pmhtech/advice/JsonpAdvice.java
@@ -5,6 +5,8 @@ import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpRespon
 
 @ControllerAdvice
 public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice{
+	
+	
 	public JsonpAdvice(){
 		super("callback");
 	}
