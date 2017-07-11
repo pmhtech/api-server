01c275ea8a55777c5afc5c85baf05bbe82e4813d
Muhyung Park <contact@pmhtech.net>
2017-07-12 06:34:53 +0900

    한명의 BJ가 두개의 방송을 켜는 케이스 해결

--- moniter/src/main/java/net/pmhtech/scheduler/MoniterScheduler.java
+++ moniter/src/main/java/net/pmhtech/scheduler/MoniterScheduler.java
@@ -2,18 +2,21 @@ package net.pmhtech.scheduler;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import net.pmhtech.mon.service.MoniterService;
 
-@Service("moniterSceduler")
+
+@Component
 public class MoniterScheduler { 
 	
 	@Resource(name="moniterService")
 	private MoniterService moniterService;
 	
+	@Scheduled(fixedDelay=1000*60*5)
     public void getLiveData(){
 		try{
 			moniterService.doLogging();
