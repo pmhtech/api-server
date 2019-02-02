54ea1b2dba552af6eca5f7b3845e4490e16a42a4
Muhyung Park <contact@pmhtech.net>
2019-02-03 01:38:08 +0900

    불필요한 파일 삭제

--- moniter/src/main/java/net/pmhtech/mon/dao/LogMstDAO.java
+++
@@ -1,7 +0,0 @@
-package net.pmhtech.mon.dao;
-
-import net.pmhtech.mon.domain.LogMst;
-
-public interface LogMstDAO {
-	public int insert(LogMst logMst) throws Exception;
-}
--- moniter/src/main/java/net/pmhtech/mon/service/ImageService.java
+++
@@ -1,7 +0,0 @@
-package net.pmhtech.mon.service;
-
-public interface ImageService {
-	
-	public void imageDownload(String owner, String imgPath) throws Exception;
-
-}
